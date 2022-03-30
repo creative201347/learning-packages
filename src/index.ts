98632b85a1eca56ee3b1e22376914119014c7b03
Nabin Dhami <creative201347@gmail.com>
2022-03-30 08:04:29 +0545

    silly mistake

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export class Log {
   static success(message: string) {
-    console.log(`%c ${message}, "color:green"`);
+    console.log(`%c ${message}`, "color:green");
   }
   static danger(message: string) {
-    console.log(`%c ${message}, "color:red"`);
+    console.log(`%c ${message}`, "color:red");
   }
   static info(message: string) {
-    console.log(`%c ${message}, "color:yellow; background:yellow"`);
+    console.log(`%c ${message}`, "color:yellow; background:yellow");
   }
 }
